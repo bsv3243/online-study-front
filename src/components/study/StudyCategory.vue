03553da03869e266bd3ee3ebcec591397885dcfc
bsv3243 <bsv3243@naver.com>
2023-01-17 23:02:32 +0900

    feat: 카테고리 radio button 에서 select 로 수정

--- src/components/study/StudyCategory.vue
+++ src/components/study/StudyCategory.vue
@@ -1,14 +1,7 @@
 <template>
   <v-sheet rounded="lg">
-    <v-radio-group>
-      <v-card-title class="ma-2">
-        <h4>분류별</h4>
-      </v-card-title>
-      <v-radio label="전체" value="ALL"></v-radio>
-      <v-radio label="취직" value="JOB"></v-radio>
-      <v-radio label="대학생" value="UNIV"></v-radio>
-      <v-radio label="어학" value="LANG"></v-radio>
-    </v-radio-group>
+      <v-checkbox label="최신순" value="recent"></v-checkbox>
+      <v-checkbox label="인원순" value="members"></v-checkbox>
   </v-sheet>
 </template>
 
--- src/components/study/StudyList.vue
+++ src/components/study/StudyList.vue
@@ -1,10 +1,69 @@
 <template>
-$END$
+  <v-sheet
+      rounded="sm"
+      min-height="70vh">
+    <v-container>
+      <v-row>
+        <v-col cols="3">
+          <v-select
+              v-model="select"
+              label="카테고리"
+              :items="categories"
+              item-title="title"
+              item-value="value"
+              density="comfortable"></v-select>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container class="d-flex">
+      <v-card
+        v-for="(group, i) in groups"
+        :key="i"
+        link>
+        <v-card-item>
+          <v-card-title v-text="group.title"></v-card-title>
+          <v-card-subtitle v-text="group.category"></v-card-subtitle>
+        </v-card-item>
+        <v-card-text>
+          {{group.memberCount}} / {{group.headcount}}
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-sheet>
 </template>
 
 <script>
 export default {
-name: "StudyList"
+  name: "StudyList",
+  data: () => ({
+    groups: [
+      {
+        title: "테스트 그룹",
+        category: "취직",
+        headcount: 30,
+        memberCount: 10,
+      }
+    ],
+    categories: [
+      {
+        title: "전체",
+        value: "ALL"
+      },
+      {
+        title: "취직",
+        value: "JOB"
+      },
+      {
+        title: "대학생",
+        value: "UNIV",
+      },
+      {
+        title: "독서",
+        value: "BOOK"
+      }
+    ],
+    select: {title: "전체", value: "ALL"}
+  })
 }
 </script>
 
