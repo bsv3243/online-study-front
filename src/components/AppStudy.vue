462f10764c89e0eb3f21a27fdf56875231a5ff4e
bsv3243 <bsv3243@naver.com>
2023-01-17 22:00:41 +0900

    feat: 카테고리 선택 추가

--- src/components/AppStudy.vue
+++ src/components/AppStudy.vue
@@ -1,14 +1,26 @@
 <template>
- <v-responsive class="mx-auto" max-width="1200px">
-   <v-container>
-
-   </v-container>
- </v-responsive>
+  <div>
+   <v-responsive class="mx-auto" max-width="1200px">
+     <v-container>
+       <v-row>
+         <v-col cols="2">
+           <StudyCategory/>
+         </v-col>
+         <v-col>
+            <StudyList/>
+         </v-col>
+       </v-row>
+     </v-container>
+   </v-responsive>
+  </div>
 </template>
 
 <script>
+import StudyCategory from "@/components/study/StudyCategory";
+import StudyList from "@/components/study/StudyList";
 export default {
-  name: "AppStudy"
+  name: "AppStudy",
+  components: {StudyList, StudyCategory}
 }
 </script>
 
---
+++ src/components/study/StudyCategory.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-sheet rounded="lg">
+    <v-radio-group>
+      <v-card-title class="ma-2">
+        <h4>분류별</h4>
+      </v-card-title>
+      <v-radio label="전체" value="ALL"></v-radio>
+      <v-radio label="취직" value="JOB"></v-radio>
+      <v-radio label="대학생" value="UNIV"></v-radio>
+      <v-radio label="어학" value="LANG"></v-radio>
+    </v-radio-group>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "StudyCategory",
+  data: () => ({
+    categories: [
+      {
+        title: "취직",
+        value: "JOB"
+      },
+      {
+        title: "대학생",
+        value: "UNIV"
+      },
+      {
+        title: "어학",
+        value: "LANG"
+      }
+    ],
+
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
