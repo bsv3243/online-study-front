b62d6ef7179c2d34e109627871dcf2ae2d680f46
bsv3243 <bsv3243@naver.com>
2023-01-28 20:43:11 +0900

    feat: 스택 차트 추가

---
+++ src/components/AppRecord.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-responsive class="mx-auto" max-width="1200px">
+      <v-container>
+        <v-row>
+          <div class="left">
+          </div>
+          <v-col>
+            <RecordList/>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-responsive>
+  </div>
+</template>
+
+<script>
+import RecordList from "@/components/record/RecordList";
+export default {
+  name: "AppRecord",
+  components: {RecordList},
+  data:() => ({
+  })
+}
+</script>
+
+<style scoped>
+.left {
+  margin-top: 12px;
+  width: 200px;
+}
+</style>
