61f81508a425fddb449e51ec5fed36e5799b602b
bsv3243 <bsv3243@naver.com>
2023-01-29 21:45:16 +0900

    feat: 스택 바, 플로팅 바 y축 라벨, 툴팁 수정

--- src/components/record/StackedBarChart.vue
+++ src/components/record/StackedBarChart.vue
@@ -18,16 +18,16 @@ export default {
   data() {
     return {
       chartData: {
-        labels: [ 'January', 'February', 'March' ],
+        labels: [ '2023-01-27', '2023-01-28', '2023-01-29' ],
         datasets: [
             {
               label: "데이터셋1",
-              data: [40, 20, 12],
+              data: [3600*3, 3600*2, 3600*4],
               backgroundColor: "#bb7e7e",
             },
           {
             label: "데이터셋2",
-            data: [20, 30, 12],
+            data: [3600, 3600*2, 3600*1.5],
             backgroundColor: "#eeba36"
           }
         ]
@@ -39,7 +39,28 @@ export default {
             stacked: true
           },
           y: {
-            stacked: true
+            stacked: true,
+            ticks: {
+              callback: (v) => {
+                return new Date(v*1000).toISOString().substring(11, 19);
+              },
+              stepSize: 120*60
+            }
+          }
+        },
+        plugins: {
+          tooltip: {
+            callbacks: {
+              label: (tooltipItem) => {
+                const index = tooltipItem.dataIndex;
+                const data = tooltipItem.dataset.data[index];
+                const date = new Date(data*1000);
+
+                const hours = date.getHours()-9;
+                const minutes = date.getMinutes();
+                return hours + "시간 " + minutes + "분";
+              }
+            }
           }
         }
       }
