268e85ab8951436dbc614af7fc83f9ac23fdb237
bsv3243 <bsv3243@naver.com>
2023-01-30 21:43:49 +0900

    feat: 상단바 수정

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -13,11 +13,6 @@
             <router-link to="/record" style="text-decoration: none; color: inherit">공부기록</router-link>
           </v-btn>
         </div>
-        <div class="ml-4">
-          <v-btn>
-            <router-link to="/record">공부 기록</router-link>
-          </v-btn>
-        </div>
         <v-spacer></v-spacer>
         <v-btn>로그인</v-btn>
       </v-container>
