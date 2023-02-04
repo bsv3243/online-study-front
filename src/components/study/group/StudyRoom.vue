a82ec98e8da8c5c46a1e8615062b41b52d527a3d
bsv3243 <bsv3243@naver.com>
2023-02-04 23:12:47 +0900

    feat: 공부 선택 추가

--- src/components/study/group/StudyRoom.vue
+++ src/components/study/group/StudyRoom.vue
@@ -2,48 +2,89 @@
   <v-container>
     <v-row class="justify-space-between">
       <v-col cols="5">
-        <v-card min-height="10vh">
+        <v-card min-height="12vh">
           <v-card-text>12명 공부 중</v-card-text>
         </v-card>
       </v-col>
       <v-col cols="5">
-        <v-card min-height="10vh">
-          <v-container>
+        <v-card min-height="12vh">
+          <v-card-text class="d-flex">
+            <p v-if="selectedStudies instanceof Array">선택된 공부가 없습니다.</p>
+            <p v-text="selectedStudies.name"></p>
+            <p v-text="selectedStudies.time"></p>
+          </v-card-text>
+          <v-card-actions>
             <v-menu
                 v-model="menu"
               :close-on-content-click="false">
               <template v-slot:activator="{props}">
                 <v-btn v-bind="props">
-                  추가
+                  변경
                 </v-btn>
               </template>
               <v-card class="pa-2" min-width="300">
-                <v-form>
-                  <v-text-field label="검색" density="compact" variant="solo">
-                  </v-text-field>
-                </v-form>
+<!--                <v-form>-->
+<!--                  <v-text-field label="검색" density="compact" variant="solo">-->
+<!--                  </v-text-field>-->
+<!--                </v-form>-->
 
-                <v-chip-group  multiple selected-class="text-primary">
-                  <v-chip v-for="study in studies" :key="study.id">
-                    {{study.name}}
-                  </v-chip>
-                </v-chip-group>
+                <v-list
+                    nav>
+                  <v-list-item
+                    v-for="study in studies"
+                    :key="study.id"
+                    :value="study.id"
+                    @click="selectedStudies=study"
+                    active-color="primary"
+                  >
+                    <v-list-item-title>{{study.name}}</v-list-item-title>
+                    <v-list-item-subtitle>{{study.time}}</v-list-item-subtitle>
+                  </v-list-item>
+                </v-list>
 
                 <v-card-actions>
+                  <v-btn variant="text" @click="test">
+                    닫기
+                  </v-btn>
                   <v-spacer/>
-                  <v-btn variant="text" @click="menu=false">
-                    취소
-                  </v-btn>
-                  <v-btn variant="text" @click="menu=false">
-                    추가
-                  </v-btn>
+                  <v-dialog max-width="350">
+                    <template v-slot:activator="{ props }">
+                      <v-btn v-bind="props">
+                        추가
+                      </v-btn>
+                    </template>
+                    <v-card>
+                      <v-card-text>
+                        <v-text-field
+                            density="compact"
+                            variant="solo"
+                            label="공부 이름"></v-text-field>
+                        <v-chip-group>
+                          <v-chip
+                              v-for="study in groupStudies"
+                              :key="study.id"
+                              @click="addStudy(study)">
+                            {{study.name}}
+                          </v-chip>
+                        </v-chip-group>
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-spacer/>
+                        <v-btn>
+                          추가하기
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+
                 </v-card-actions>
               </v-card>
             </v-menu>
+            <v-spacer/>
             <v-btn>
               시작
             </v-btn>
-          </v-container>
+          </v-card-actions>
         </v-card>
       </v-col>
     </v-row>
@@ -68,8 +109,7 @@ export default {
   name: "StudyRoom",
   components: {StudyMember},
   watch: {
-    selectedStudies(value) {
-      console.log(value);
+    notSelected() {
     }
   },
   data: () => ({
@@ -105,16 +145,35 @@ export default {
     studies: [
       {
         id: "1",
-        name: "스프링"
+        name: "스프링",
+        time: "3시간 30분"
       },
       {
         id: "2",
-        name: "백엔드"
+        name: "백엔드",
+        time: "2시간 12분"
       }
     ],
     selectedStudies: [],
+    groupStudies: [
+      {
+        id: "3",
+        name: "레디스"
+      },
+      {
+        id: "4",
+        name: "리액트"
+      }
+    ]
   }),
-  mounted() {
+  methods: {
+    test() {
+      this.menu = false;
+    },
+    addStudy(study) {
+      study.time = "0시간 0분"
+      this.studies.push(study)
+    }
   }
 }
 </script>
