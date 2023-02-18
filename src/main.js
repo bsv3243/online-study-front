b2775d5f564a56b6b1274f5d0429413e9ab6dc35
bsv3243 <bsv3243@naver.com>
2023-02-18 20:59:56 +0900

    vue-cookie 라이브러리 추가, withCredentials 전역설정 추가

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import router from "./router/router"
 import axios from 'axios'
+import VueCookies from "vue-cookies"
 
 loadFonts()
 
 const app = createApp(App);
 
+axios.defaults.withCredentials = true
+
 app.config.globalProperties.axios = axios;
-app.use(router).use(vuetify).mount('#app')
+app.use(router).use(VueCookies).use(vuetify).mount('#app')
 
 // createApp(App)
 //     .use(router)
