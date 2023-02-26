596d46c75bdd5a7cae35d138b861a6aa0fa1726c
bsv3243 <bsv3243@naver.com>
2023-02-26 22:17:57 +0900

    api: 그룹 조회 요청 추가

--- src/components/study/StudyGroup.vue
+++ src/components/study/StudyGroup.vue
@@ -27,7 +27,7 @@
             </v-list>
           </v-col>
           <v-col>
-            <StudyGroupInfo/>
+            <StudyGroupInfo :group="group"/>
             <StudyRoom v-if="select==='room'"/>
             <StudyCommunity v-if="select==='community'"/>
           </v-col>
@@ -47,6 +47,9 @@ import StudyGroupInfo from "@/components/study/group/StudyGroupInfo";
 export default {
   name: "StudyGroup",
   components: {SvgIcon, StudyGroupInfo, StudyCommunity, StudyRoom},
+  mounted() {
+    this.groupId = this.$route.params.groupId;
+  },
   watch: {
     select(value) {
       console.log(value);
@@ -56,9 +59,38 @@ export default {
     select: "room",
     isMember: false,
     joinedAt: "2023-01-23",
+    groupId: null,
+    dataReady: false,
     mdiDeskLampOn,
+
+    //axiosResponse 데이터
+    group: {
+      groupId: 0,
+      name: "테스트그룹",
+      headcount: 30,
+      description: "중요한 것은 꺾이지 않는 마음",
+      createdAt: new Date(),
+      category: "기타",
+      groupMembers: [
+        {
+          groupMemberId: 0,
+          memberId: 0,
+          username: "tester",
+          nickname: "tester",
+          joinedAt: new Date(),
+          role: "MASTER"
+        }
+      ],
+      rooms: []
+    }
   }),
   methods: {
+    async groupGetApiCall() {
+      const response = await this.axios.get("http://localhost:8080/api/v1/group/" + this.groupId);
+      this.group = response.data.data;
+
+      console.log(this.group)
+    }
   }
 }
 </script>
