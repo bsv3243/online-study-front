b797d3f0bf2d22f686a20629e0be1f0dbab9629d
bsv3243 <bsv3243@naver.com>
2023-03-11 02:13:41 +0900

    feat: 기록 계산용 ticket api call 추가

--- src/components/record/RecordList.vue
+++ src/components/record/RecordList.vue
@@ -6,7 +6,7 @@
           <v-card class="pa-3">
               총 공부시간 합산 차트
             <v-card-text>
-              <PieChart/>
+              <PieChart :member-ticket="memberTicket" v-if="dataReady"/>
             </v-card-text>
           </v-card>
         </v-col>
@@ -16,7 +16,7 @@
           <v-card class="pa-3" min-height="35vh">
             공부시간 차트
             <div style="height: 30vh">
-              <StackedBarChart/>
+              <StackedBarChart :member-ticket="memberTicket" v-if="dataReady"/>
             </div>
           </v-card>
         </v-col>
@@ -24,7 +24,7 @@
           <v-card class="pa-3" min-height="35vh">
             공부 시작 종료 시간 차트
             <div style="height: 30vh">
-            <FloatingBarChar/>
+            <FloatingBarChar :member-ticket="memberTicket" v-if="dataReady"/>
             </div>
           </v-card>
         </v-col>
@@ -37,9 +37,90 @@
 import StackedBarChart from "@/components/record/StackedBarChart";
 import FloatingBarChar from "@/components/record/chart/FloatingBarChar";
 import PieChart from "@/components/record/chart/PieChart";
+import {useMemberStore} from "@/store/MemberStore";
+import {useLoginStore} from "@/LoginStore";
 export default {
   name: "RecordList",
-  components: {PieChart, FloatingBarChar, StackedBarChart}
+  components: {PieChart, FloatingBarChar, StackedBarChart},
+  setup() {
+    const memberStore = useMemberStore();
+    const loginStore = useLoginStore();
+
+    return {memberStore, loginStore};
+  },
+  async mounted() {
+    await this.ticketGetApiCall(7)
+  },
+  data:() => ({
+    //axios
+    //request
+    ticketGetRequest: {
+      date: new Date(),
+      days: 7,
+    },
+    //response
+    memberTicket: {
+      memberId: 1,
+      nickname: "member",
+      studyTime: 0,
+      activeTicket: Object,
+      expiredTickets: [
+        {
+          ticketId: 1,
+          status: "STUDY",
+          activeTime: 3600,
+          startTime: new Date().toISOString(),
+          endTime: new Date().toISOString(),
+          study: {
+            studyId: 1,
+            name: "study"
+          }
+        }
+      ]
+    },
+    ticket: {
+      ticketId: 1,
+      status: "STUDY",
+      activeTime: 3600,
+      startTime: new Date().toISOString(),
+      endTime: new Date().toISOString(),
+      study: {
+        studyId: 1,
+        name: "study"
+      }
+    },
+    //complete
+    dataReady: false,
+  }),
+  methods: {
+    async ticketGetApiCall(days) {
+      this.ticketGetRequest.days = days;
+      let date = this.ticketGetRequest.date;
+      date.setDate(date.getDate()-days)
+
+      if(!this.loginStore.isLogin) {
+        this.dataReady = true
+        return
+      }
+      try {
+        const response = await this.axios.get("http://localhost:8080/api/v1/tickets", {
+          params: {
+            date: date.toISOString().substring(0, 10),
+            days: days
+          }
+        });
+
+        this.memberTicket = response.data.data
+
+        console.log(response.data.data)
+
+        this.dataReady = true;
+      } catch (err) {
+        alert("잠시 후에 다시 시도해주세요.")
+        console.log(err)
+      }
+    }
+  },
 }
 </script>
 
