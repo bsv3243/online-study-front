a7750633239e8a032fe25556b0778aa946a9986f
hseong3243 <hseong3243@gmail.com>
2023-03-23 23:36:54 +0900

    feat: 타임라인 차트 추가

--- src/components/record/RecordList.vue
+++ src/components/record/RecordList.vue
@@ -2,41 +2,47 @@
     <v-container>
       <v-row>
         <v-col cols="6">
-          <v-card class="pa-3">
-              총 공부시간 합산 차트
-            <v-card-text>
-              <PieChart :study-records="studyRecords" v-if="dataReady && isRecordExist" :key="chartKey"/>
-              <div class="d-flex justify-center align-center h-100">
-                <p v-if="!isRecordExist">데이터 없음</p>
+          <div class="record-container">
+            <v-card class="pa-3">
+              <strong>총 학습 시간</strong>
+              <v-card-text>
+                <PieChart :study-records="studyRecords" v-if="dataReady && isRecordExist" :key="chartKey"/>
+                <div class="d-flex justify-center align-center h-100">
+                  <p v-if="!isRecordExist">데이터 없음</p>
+                </div>
+              </v-card-text>
+            </v-card>
+            <v-card class="pa-3" min-height="35vh">
+              <strong>개별 학습 시간</strong>
+              <div style="height: 30vh">
+                <StackedBarChart :study-records="studyRecords"
+                                 :study-records-others="studyRecordsOthers"
+                                 v-if="dataReady && isRecordExist" :key="chartKey"/>
+                <div class="d-flex justify-center align-center h-100">
+                  <p v-if="!isRecordExist">데이터 없음</p>
+                </div>
               </div>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="6">
-          <v-card class="pa-3" min-height="35vh">
-            공부시간 차트
-            <div style="height: 30vh">
-              <StackedBarChart :study-records="studyRecords"
-                               :study-records-others="studyRecordsOthers"
-                               v-if="dataReady && isRecordExist" :key="chartKey"/>
-              <div class="d-flex justify-center align-center h-100">
-                <p v-if="!isRecordExist">데이터 없음</p>
-              </div>
-            </div>
-          </v-card>
+            </v-card>
+          </div>
         </v-col>
         <v-col cols="6">
-          <v-card class="pa-3" min-height="35vh">
-            공부 시작 종료 시간 차트
-            <div style="height: 30vh">
-              <scatter-chart :study-records="studyRecords" v-if="dataReady && isRecordExist" :key="chartKey"/>
-              <div class="d-flex justify-center align-center h-100">
-                <p v-if="!isRecordExist">데이터 없음</p>
+          <div class="record-container">
+            <v-card class="pa-3" min-height="35vh">
+              <strong>학습 시작 종료 시간</strong>
+              <div style="height: 30vh">
+                <scatter-chart :study-records="studyRecords" v-if="dataReady && isRecordExist" :key="chartKey"/>
+                <div class="d-flex justify-center align-center h-100">
+                  <p v-if="!isRecordExist">데이터 없음</p>
+                </div>
               </div>
-            </div>
-          </v-card>
+            </v-card>
+            <v-container class="pa-3">
+              <div style="height: 39vh">
+
+                <record-time-line :times="times" :selected-study="selectedStudy"/>
+              </div>
+            </v-container>
+          </div>
         </v-col>
       </v-row>
     </v-container>
@@ -49,54 +55,54 @@ import {useMemberStore} from "@/store/MemberStore";
 import {useLoginStore} from "@/LoginStore";
 import ScatterChart from "@/components/record/chart/ScatterChart";
 import moment from "moment";
+import RecordTimeLine from "@/components/record/RecordTimeline";
+
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+import {ko} from 'date-fns/locale'
 
 export default {
   name: "RecordList",
-  components: {ScatterChart, PieChart, StackedBarChart},
+  components: {RecordTimeLine, ScatterChart, PieChart, StackedBarChart, VueDatePicker},
   props: {
     times: Object,
     selectedStudy: Object
   },
   watch: {
-      async selectedStudy() {
-        if(this.selectedStudy) {
-          this.recordGetRequest.studyId = this.selectedStudy.studyId
-        } else {
-          this.recordGetRequest.studyId = null;
-        }
+    async selectedStudy() {
+      if (this.selectedStudy) {
+        this.recordGetRequest.studyId = this.selectedStudy.studyId
+      } else {
+        this.recordGetRequest.studyId = null;
+      }
 
-        this.recordGetRequest.memberId = this.memberStore.getMemberId;
-        this.studyRecords = await this.recordGetApiCall()
+      this.recordGetRequest.memberId = this.memberStore.getMemberId;
+      this.studyRecords = await this.recordGetApiCall()
 
-        console.log(this.studyRecords)
-        
-        if(this.selectedStudy) { // 특정 스터디가 선택되었을 때 다른 사용자들의 기록을 요청한다.
-          this.recordGetRequest.memberId = null;
-          this.studyRecordsOthers = await this.recordGetApiCall()
-        } else {
-          this.studyRecordsOthers = null;
-        }
+      console.log(this.studyRecords)
 
-        this.isRecordExist = this.studyRecords.length !== 0;
+      if (this.selectedStudy) { // 특정 스터디가 선택되었을 때 다른 사용자들의 기록을 요청한다.
+        this.recordGetRequest.memberId = null;
+        this.studyRecordsOthers = await this.recordGetApiCall()
+      } else {
+        this.studyRecordsOthers = null;
+      }
 
-        this.chartKey++;
-      },
+      this.isRecordExist = this.studyRecords.length !== 0;
+
+      this.chartKey++;
+    },
     async times() {
 
-      this.recordGetRequest.memberId = this.memberStore.getMemberId
-      this.recordGetRequest.startDate = moment(this.times.startTime).format().substring(0, 10);
 
-
-      let millis = this.times.endTime.getTime() - this.times.startTime.getTime();
-      this.recordGetRequest.days = millis / (1000 * 3600 * 24) + 1;
-
-      this.studyRecords = await this.recordGetApiCall()
+      this.recordGetRequest.memberId = this.memberStore.getMemberId;
+      this.seStartDateAndDays();
+      this.studyRecords = await this.recordGetApiCall();
 
       this.isRecordExist = this.studyRecords.length !== 0;
 
       this.chartKey++;
     }
-
   },
   setup() {
     const memberStore = useMemberStore();
@@ -114,6 +120,8 @@ export default {
     this.dataReady = true;
   },
   data:() => ({
+    ko:ko,
+    timelineDate: null,
     isRecordExist: true,
     chartKey: 0,
     //axios
@@ -143,11 +151,24 @@ export default {
         alert("잠시 후에 다시 시도해주세요.");
         console.log(err);
       }
+    },
+    seStartDateAndDays() {
+      this.recordGetRequest.startDate = moment(this.times.startTime).format().substring(0, 10);
+
+      let millis = this.times.endTime.getTime() - this.times.startTime.getTime();
+      this.recordGetRequest.days = millis / (1000 * 3600 * 24) + 1;
     }
   },
 }
 </script>
 
 <style scoped>
-
+.record-container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+.timeline-date-picker {
+  width: 60%;
+}
 </style>
---
+++ src/components/record/RecordTimeline.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="gap-container">
+    <div class="d-flex justify-space-between">
+      <strong>타임라인</strong>
+      <vue-date-picker class="date-picker"
+                       v-model="date"
+                       :format-locale="ko"
+                       format="Y-MM-dd"
+                       :max-date="new Date()"
+                       :enable-time-picker="false"/>
+    </div>
+      <v-timeline side="end" align="start">
+        <v-timeline-item v-for="ticket of ticketDtos"
+                         :key="ticket.startTime"
+                         :dot-color="getColor(ticket.status)"
+                         size="small">
+          <div class="d-flex">
+            <strong class="me-4">{{ticket.startTime}} - {{ticket.endTime}}</strong>
+            <div>
+              <strong>{{getStudyNameOrRest(ticket)}}</strong>
+            </div>
+          </div>
+        </v-timeline-item>
+      </v-timeline>
+  </div>
+</template>
+
+<script>
+import {useMemberStore} from "@/store/MemberStore";
+import moment from "moment";
+
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+import {ko} from 'date-fns/locale'
+
+export default {
+  name: "RecordTimeLine",
+  components: {
+    VueDatePicker,
+  },
+  props: {
+    times: Object,
+    selectedStudy: Object,
+  },
+  setup() {
+    const memberStore = useMemberStore();
+
+    return {memberStore}
+  },
+  data:() => ({
+    date: null,
+    ko: ko,
+    ticketGetRequest: {
+      groupId: null,
+      studyId: null,
+      memberId: null,
+      date: null,
+      days: null,
+    },
+    memberTickets: [],
+    expiredTickets: [],
+    ticketDto: {
+      startTime: null,
+      endTime: null,
+      studyName: null,
+      status: null,
+    },
+    ticketDtos: [],
+  }),
+  watch: {
+    async date() {
+      this.initTicketRequest()
+
+      this.memberTickets = await this.ticketGetApiCall()
+
+      console.log(this.memberTickets);
+
+      this.setData()
+    }
+  },
+  async mounted() {
+    this.initTicketRequest()
+
+    this.memberTickets = await this.ticketGetApiCall()
+
+    this.setData()
+  },
+  methods: {
+    initTicketRequest() {
+      this.ticketGetRequest.memberId = this.memberStore.getMemberId;
+
+      if(this.date) {
+        this.ticketGetRequest.date = this.date.toISOString().substring(0, 10);
+      }
+
+      if(this.selectedStudy) {
+        this.ticketGetRequest.studyId = this.selectedStudy.studyId;
+      }
+
+      if(this.times.startTime) {
+        this.ticketGetRequest.startDate = moment(this.times.startTime).format().substring(0, 10);
+
+        let millis = this.times.endTime.getTime() - this.times.startTime.getTime();
+        this.ticketGetRequest.days = millis / (1000 * 3600 * 24) + 1;
+      }
+    },
+    async ticketGetApiCall() {
+      try {
+        const response = await this.axios.get("/api/v1/tickets", {
+          params: this.ticketGetRequest
+        });
+
+        return response.data.data;
+      }
+      catch (err) {
+        alert("잠시 후에 다시 시도해주세요.");
+        console.log(err);
+      }
+    },
+    setData() {
+      this.ticketDtos = []
+
+      this.expiredTickets = this.memberTickets[0].expiredTickets
+
+      this.expiredTickets.forEach(expiredTicket => {
+        this.ticketDto = {
+          startTime: expiredTicket.startTime.substring(11, 16),
+          endTime: expiredTicket.endTime.substring(11, 16),
+          studyName: expiredTicket.study.name,
+          status: expiredTicket.status,
+        }
+
+        this.ticketDtos.push(this.ticketDto)
+      })
+    },
+    getColor(status) {
+      if(status === "STUDY") {
+        return "pink"
+      } else {
+        return "teal-lighten-3"
+      }
+    },
+    getStudyNameOrRest(ticket) {
+      if(ticket.status === "STUDY") {
+        return ticket.studyName
+      } else {
+        return "휴식";
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+.gap-container {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.date-picker {
+  width: 60%;
+  align-self: center;
+}
+</style>
