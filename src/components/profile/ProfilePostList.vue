eb8bf572ec960d1ba8f12e8bda9d8215f6ea4ac7
hseong3243 <hseong3243@gmail.com>
2023-04-07 19:18:36 +0900

    feat: 게시글 관리 추가

--- src/components/profile/ProfilePostList.vue
+++ src/components/profile/ProfilePostList.vue
@@ -2,43 +2,121 @@
   <div class="base-container">
     <div class="title">
       <span>게시글 관리</span>
-      <v-pagination length="5"></v-pagination>
+      <v-pagination v-model="page" :length="pageInfo.totalPages" total-visible="5"></v-pagination>
     </div>
-    <div class="group-container">
+    <div class="group-container" v-if="dataReady">
+      <div class="card-container" v-for="post in posts" :key="post.postId">
       <div class="group-card">
-        <span>게시글 제목</span>
+        <span>{{ post.title }}</span>
         <div class="post-info">
           <div>
             <span>조회수</span>
-            <span>1</span>
+            <span>{{ post.viewCount }}</span>
           </div>
           <div>
             <span>댓글</span>
-            <span>1</span>
+            <span>{{ post.comments.length }}</span>
           </div>
         </div>
-        <span>작성일 2023-04-05</span>
+        <span>작성일 {{ getYMD(post.createdAt) }}</span>
         <div class="card-action">
-          <v-btn variant="tonal">바로가기</v-btn>
+          <v-btn @click="moveTo(post.postId)" variant="tonal">바로가기</v-btn>
           <v-btn variant="tonal">삭제</v-btn>
         </div>
       </div>
+      </div>
     </div>
     <div class="align-self-end">
-      <v-btn variant="tonal">전체 삭제</v-btn>
+      <v-btn @click="deletePosts" variant="tonal">전체 삭제</v-btn>
     </div>
   </div>
 </template>
 
 <script>
+import {useLoginStore} from "@/store/LoginStore";
+import moment from "moment";
+
 export default {
   name: "ProfilePostList",
+  setup() {
+    const loginStore = useLoginStore();
+
+    return {loginStore}
+  },
+  data:() => ({
+    memberId: null,
+    page: 1,
+    posts: [{
+      postId: 1,
+      title: "게시글제목",
+      viewCount: 1,
+      comments:[],
+      createdAt: new Date().toISOString(),
+    }],
+    postsGetRequest: {
+      page: 0,
+      size: 4,
+      memberId: null,
+    },
+    pageInfo: {
+      number: 0,
+      size: 4,
+      totalPages: 1,
+      hasNext: false,
+      hasPrevious: false
+    },
+    dataReady: false,
+  }),
+  watch: {
+    page() {
+      this.init();
+    }
+  },
+  mounted() {
+    this.init()
+  },
   methods: {
-    async postsGetApiCall() {
+    async init() {
+      this.memberId = this.loginStore.getMemberId;
+
+      this.postsGetRequest.memberId = this.memberId
+      this.postsGetRequest.page = this.page-1
+      this.posts = await this.postsGetApiCall(this.postsGetRequest)
+
+      this.dataReady = true
+    },
+    async postsGetApiCall(request) {
       try {
-        const response = await this.axios.get("/api/v1/posts");
+        const response = await this.axios.get("/api/v1/posts", {
+          params: request
+        });
+
+        this.pageInfo = response.data;
+
+        console.log(this.pageInfo)
 
         return response.data.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    getYMD(createdAt) {
+      return moment(createdAt).format().substring(0, 10);
+    },
+    moveTo(postId) {
+      this.$router.push("/community/"+postId)
+    },
+    async deletePosts() {
+      const request = {
+        memberId: this.memberId
+      }
+      await this.deletePostsApiCall(request);
+
+      await this.init()
+    },
+    async deletePostsApiCall(request) {
+      try {
+        await this.axios.post("/api/v1/posts/delete", request)
       } catch (err) {
         console.log(err);
       }
@@ -62,20 +140,23 @@ export default {
 .group-container {
   display: flex;
 }
+.card-container {
+  padding: 5px;
+  width: 25%;
+}
 .group-card {
   display: flex;
   flex-direction: column;
   gap: 5px;
 
-  width: 25%;
-  padding: 15px;
+  padding: 10px;
 
   border: 2px solid #efefef;
   border-radius: 10px;
 }
 .group-card .card-action {
   display: flex;
-  gap: 10px;
+  gap: 5px;
 }
 .post-info {
   display: flex;
