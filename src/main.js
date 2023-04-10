a40c5cc0ae7532657947c3592989befa0ebafc88
hseong3243 <hseong3243@gmail.com>
2023-04-10 21:33:11 +0900

    feat: axios 인터셉터 설정 추가 및 배포를 위한 baseURL 변경

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import VueCookies from "vue-cookies"
 import {createPinia} from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import GlobalPlugin from "@/method/GlobalPlugin";
-
+import {useLoginStore} from "@/store/LoginStore";
 
 loadFonts()
 
@@ -17,7 +17,32 @@ pinia.use(piniaPluginPersistedstate)
 const app = createApp(App);
 
 axios.defaults.withCredentials = true
-axios.defaults.baseURL = "http://localhost:8080"
+axios.defaults.baseURL = "https://studybread.shop"
+// axios.defaults.baseURL = "http://localhost:8080"
+
+
+axios.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if(error.response && error.response.status) {
+            const loginStore = useLoginStore();
+            switch (error.response.status) {
+                //401의 경우 메인화면 로그인 모달
+                case 401:
+                    loginStore.logout()
+                    loginStore.setLoginDialog(true)
+/*                    console.log(loginStore.getLoginDialog)
+                    router.push("/")*/
+                    return new Promise(() => {})
+                default:
+                    return Promise.reject(error)
+            }
+        }
+        return Promise.reject(error)
+    }
+)
 
 app.config.globalProperties.axios = axios;
 app
